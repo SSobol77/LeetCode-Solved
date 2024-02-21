531c047fa42e6bd328d3ae1c12b10bd8604a6f66
Siergej Sobolewski <108773983+SSobol77@users.noreply.github.com>
2024-02-21 20:19:19 +0100

    solution

---
+++ Cpp/141. Linked List Cycle.cpp
@@ -0,0 +1,111 @@
+// 141. Linked List Cycle.
+
+
+// Topic: Hash Table, Linked List, Two Pointers/
+
+
+/*
+### Task:
+---
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Constraints:
+The number of the nodes in the list is in the range [0, 10^4].
+-10^5 <= Node.val <= 10^5
+pos is -1 or a valid index in the linked-list.
+ 
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+
+### Testcase:
+---
+[3,2,0,-4]
+1
+[1,2]
+0
+[1]
+-1
+
+
+### Code:
+---
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ *//*
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+    }
+};
+*/
+
+
+// Solution: --------------------------------------
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) {
+            return false; // No cycle if there are 0 or 1 nodes
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while (slow != fast) {
+            if (!fast || !fast->next) {
+                return false; // Reached end of list, no cycle
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return true; // Cycle detected
+    }
+};
+
+// Description: ===================================
+/*
+To determine if a linked list has a cycle, we can use the "Two Pointers" approach. We'll use two pointers, a slow pointer and 
+a fast pointer. The slow pointer moves one step at a time, while the fast pointer moves two steps at a time. If there is a cycle, 
+eventually the fast pointer will catch up to the slow pointer.
+
+This solution has a time complexity of O(N), where N is the number of nodes in the linked list, and a space complexity of O(1), 
+since we only use constant extra space regardless of the size of the input linked list.
+
+*/
